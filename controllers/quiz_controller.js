b2da91a2ad2a3a0d3a6af1d72852df5c2c5fa0aa
jacanestro <jacanestro@yahoo.es>
2015-07-11 09:52:56 +0100

    Paso 10: Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,27 +7,36 @@ exports.question = function(req,res) {
 	})
 };
 */
+exports.load = function(req,res,next,quizId) {
+	models.Quiz.find(quizId).then (
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			}
+			else {
+				next(new Error('No existe el quiId=' + quizId));
+			}
+		}
+	).catch(function(error) { next(error);});
+};
+
 exports.index = function(req,res) {
 	models.Quiz.findAll().then(function(quizes) {
 		res.render('quizes/index.ejs', { quizes:quizes });
-	})
+	}).catch(function(error) { next(error);})
 };
 
 exports.show = function(req,res) {
-    models.Quiz.find(req.params.quizId).then(function(quiz) {
-    	res.render('quizes/show', { quiz: quiz });
-    })
+  	res.render('quizes/show', { quiz: req.quiz });
 };
 
 exports.answer = function(req,res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-	  if (req.query.respuesta === quiz.respuesta) {
-	    res.render('quizes/answer', { quiz: quiz, respuesta: 'correcta' });
+	var resultado = 'incorrecta';
+	  if (req.query.respuesta === req.quiz.respuesta) {
+	  	resultado = 'correcta';
 	  }
-	  else {
-	    res.render('quizes/answer', { quiz: quiz, respuesta: 'incorrecta' });
-	  }
-	})
+	  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
 };
 
 exports.author = function(req, res) {
